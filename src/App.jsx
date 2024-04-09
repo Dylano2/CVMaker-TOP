8af26ac328dd5387407fe2d9a51867e973c323b2
Dylan <dylan.gonzalez@outlook.fr>
2024-04-09 09:38:29 +0200

    removed personal todo list

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,6 @@ function App() {
   const toggleData = false
   return (
     <>
-      <div className={'flex flex-col absolute'}>
-        <label>
-          <input type={'checkbox'} checked={colorPicker} />
-          {' Faire un color picker '}
-        </label>{' '}
-        <label>
-          <input type={'checkbox'} checked={randomizedData} />
-          {' Randomisez la data du cv '}
-        </label>{' '}
-        <label>
-          <input type={'checkbox'} checked={draggableElement} />
-          {' element draggable '}
-        </label>{' '}
-        <label>
-          <input type={'checkbox'} checked={web2Pdf} />
-          {' export possible en pdf '}
-        </label>{' '}
-        <label>
-          <input type={'checkbox'} checked={toggleData} />
-          {' ajouter / supprimer des data '}
-        </label>
-        <input type="color" />
-      </div>
       <CVFile />
     </>
   )
